e0e7bb4a12dad80315603d6e66b75bf4d85bc27d
tharun5343 <tharun5343@gmail.com>
2025-06-07 17:19:15 +0530

    add error handling for height and weight

--- bmi-calculator/script.js
+++ bmi-calculator/script.js
@@ -4,8 +4,16 @@ form.addEventListener("submit", (e) => {
 
   const height = parseInt(document.getElementById("height").value) / 100;
   const weight = parseInt(document.getElementById("weight").value);
-  const bmi = weight / height ** 2;
 
   const result = document.getElementById("result");
-  result.innerText = bmi.toFixed(1);
+  const error = document.getElementById("error");
+
+  if (height === "" || height < 0 || isNaN(height)) {
+    error.innerHTML = "Please give a valid height";
+  } else if (weight === "" || weight < 0 || isNaN(weight)) {
+    error.innerHTML = "Please give a valid weight";
+  } else {
+    const bmi = weight / height ** 2;
+    result.innerText = bmi.toFixed(1);
+  }
 });
