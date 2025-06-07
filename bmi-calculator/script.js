3bf32ab7ba692a926ab51fc5f406ec8d075bb929
tharun5343 <tharun5343@gmail.com>
2025-06-07 21:02:46 +0530

    improve validation logic

--- bmi-calculator/script.js
+++ bmi-calculator/script.js
@@ -1,4 +1,5 @@
 const form = document.querySelector("form");
+
 form.addEventListener("submit", (e) => {
   e.preventDefault();
 
@@ -10,10 +11,27 @@ form.addEventListener("submit", (e) => {
 
   if (height === "" || height < 0 || isNaN(height)) {
     error.innerHTML = "Please give a valid height";
-  } else if (weight === "" || weight < 0 || isNaN(weight)) {
-    error.innerHTML = "Please give a valid weight";
-  } else {
-    const bmi = weight / height ** 2;
-    result.innerText = bmi.toFixed(1);
+    return;
   }
+
+  if (!weight === "" || weight < 0 || isNaN(weight)) {
+    error.innerHTML = "Please give a valid weight";
+    return;
+  }
+
+  const bmi = weight / height ** 2;
+  let hexCode;
+
+  if (bmi < 18.6) {
+    hexCode = "#7b9df4";
+  } else if (bmi < 24.9) {
+    hexCode = "#49c5b6";
+  } else if (bmi < 39.9) {
+    hexCode = "#f7b94c";
+  } else {
+    hexCode = "#f97b77";
+  }
+
+  result.style.color = hexCode;
+  result.textContent = bmi.toFixed(1);
 });
